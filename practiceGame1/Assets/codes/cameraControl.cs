e964a4a18ef0a8d21a46eb1adc2dcae49b4936f7
Can_Sahin <cansahin98@gmail.com>
2020-09-03 16:12:51 +0300

    First innitial commit

---
+++ practiceGame1/Assets/codes/cameraControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraControl : MonoBehaviour
+{
+    public GameObject positionOfBall;
+
+    Vector3 distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        distance = transform.position - positionOfBall.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = positionOfBall.transform.position + distance;
+    }
+}
---
+++ practiceGame1/Assets/codes/codeOfBall.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class codeOfBall : MonoBehaviour
+{
+    public Text counterText;
+    public Text gameOverText;
+
+    
+    Rigidbody pyhsical;
+    int counter = 0;
+    const int targetCount = 13;
+
+    public int speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pyhsical = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontalMove = Input.GetAxisRaw("Horizontal");
+        float verticalMove = Input.GetAxisRaw("Vertical");
+
+        Vector3 movementVector = new Vector3(horizontalMove, 0, verticalMove);
+
+        pyhsical.AddForce(movementVector * speed);
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "myTargets")
+        {
+            other.gameObject.SetActive(false);
+            counter++;
+
+            counterText.text = "Score : " + counter;
+
+            if (counter == targetCount)
+            {
+                gameOverText.text = "    Well Done!";
+
+            }
+            
+        }
+    
+    }
+
+}
